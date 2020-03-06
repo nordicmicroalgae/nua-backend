6ed9329328bbd8b390f8bc63b0c1a6781f06a1ad
Andreas Loo <andreas.loo@smhi.se>
2020-03-06 13:32:46 +0100

    Read settings from environment
    SECRET_KEY and DEBUG is read from environment variables

--- nordicmicroalgae/settings.py
+++ nordicmicroalgae/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '161kyr9it@ha@zpsbr*)zl6%@yz)p1!-0(=(!$y-7#(6(+o90g'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = []
 
